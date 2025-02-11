20a4423f189e5a1cb2786b867cb141517eb2df7b
parjinder <parjinderpannu@gmail.com>
2025-02-10 22:48:51 -0800

    testing some basic of golang

---
+++ hw.go
@@ -0,0 +1,14 @@
+package main
+
+// import "fmt"
+
+import (
+	"fmt"
+)
+
+// start of execution is package main func main
+// if func Main : capital then it is exported otherwise it is internal function. It is enforced by compiler.
+// when you save go code IDE runs formating
+func main() {
+	fmt.Println("Hello Gophers :-) \u263A")
+}
