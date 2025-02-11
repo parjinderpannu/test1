9bebbe5c98d35a3bfbc83e6fbfcd1be730b7c52a
parjinder <parjinderpannu@gmail.com>
2025-02-11 06:51:52 -0800

    fixed the padding for unicode in go code

--- banner/banner.go
+++ banner/banner.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"fmt"
+	"unicode/utf8"
 )
 
 func main() {
 	banner("GO", 20)
+	banner("G😀", 20)
+
+	s := "G😀"
+	fmt.Println("len:", len(s))
+
+	for i, v := range s {
+		fmt.Println(i, v)
+	}
+
+	// strings are represented by two types
+	// byte (uint8)
+	// rune (int32)
+
 }
 
 func banner(text string, width int) {
-	padding := (width - len(text)) / 2
+	padding := (width - utf8.RuneCountInString(text)) / 2
+	// padding := (width - len(text)) / 2
 
 	for i := 0; i < padding; i++ {
 		fmt.Print(" ")
