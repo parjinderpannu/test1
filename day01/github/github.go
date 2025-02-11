ebf3e758f76428b21697a9af7083370e00c2a0cd
parjinder <parjinderpannu@gmail.com>
2025-02-11 09:32:27 -0800

    decode and unmarshalling

--- day01/github/github.go
+++ day01/github/github.go
@@ -1,11 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
@@ -21,17 +20,37 @@ func main() {
 		log.Fatalf("error:%s", resp.Status)
 	}
 	fmt.Printf("Content-Type: %s\n", resp.Header.Get("Content-Type"))
-	if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
-		log.Fatal("error: can't copy - %s", err)
+	/*
+		if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
+			log.Fatal("error: can't copy - %s", err)
+		}
+	*/
+
+	var r Reply
+	dec := json.NewDecoder(resp.Body)
+	if err := dec.Decode(&r); err != nil {
+		log.Printf("error: can't decode - %s", err)
 	}
+	fmt.Printf("%#v\n", r)
+}
+
+type Reply struct {
+	Login        string
+	Public_Repos int
 }
 
 /*
-JSON <--> GO
+JSON <--> GO [serialization or unmarshalling]
 true/false <-->
 string <-->
 null <--> nil
 number <--> float64, float32, int8..16..32..64, int, uint8, ...
-array <--> []any ([]interface{})
+array <--> []any ([]interface{}) slice of some type
 object <--> map[string]any, struct
+
+encoding/json API
+JSON --> io.Reader --> Go: json.Decoder
+JSON --> []byte --> Go: json.Unmarshal
+Go --> io.Writer --> JSON: json.Encoder
+Go --> []byte --> JSON: json:Marshal
 */
