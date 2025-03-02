81d35d3ecf4f23a44b4efdc46a68773e3aba8bda
parjinder <parjinderpannu@gmail.com>
2025-03-02 12:12:14 -0800

    for loop func print i variable fine

--- practise/go_chan/go_chan.go
+++ practise/go_chan/go_chan.go
@@ -9,6 +9,12 @@ func main() {
 	go fmt.Println("goroutine")
 	fmt.Println("Inside main without go routine")
 
+	for i := 0; i < 3; i++ {
+		func() {
+			fmt.Println("For loop i: ", i)
+		}()
+	}
+
 	time.Sleep(10 * time.Millisecond)
 	// main go routine terminate before other go routine finishes
 }
