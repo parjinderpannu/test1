b07f9f578ca4360abf1fd6096727bdd33a6f285f
parjinder <parjinderpannu@gmail.com>
2025-03-03 10:16:37 -0800

    atomic solution to avoid race condition

--- day03/counter/counter.go
+++ day03/counter/counter.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
 )
 
 func main() {
+	/* solution 1: use mutex
 	var mu sync.Mutex
 	count := 0
+	*/
+	// count := int64(0)
+	var count int64
+
 	const n = 10
 	var wg sync.WaitGroup
 	wg.Add(n)
@@ -15,9 +21,12 @@ func main() {
 		go func() {
 			defer wg.Done()
 			for j := 0; j < 10_000; j++ {
-				mu.Lock()
-				count++
-				mu.Unlock()
+				/*
+					mu.Lock()
+					count++
+					mu.Unlock()
+				*/
+				atomic.AddInt64(&count, 1)
 			}
 		}()
 	}
